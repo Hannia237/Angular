f3b9153ae05bd69ffa0fd6771422041399fe999a
Hannia237 <hch06263@gmail.com>
2022-11-21 19:24:34 -0600

    Videos 6-11

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -3,9 +3,57 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-empleado',
   templateUrl: './empleado.component.html',
+  //template: "<p>Aquí iría un empleado</p>",
   styleUrls: ['./empleado.component.css']
+  //styles: ["p{background-color:red};"]
+
 })
 export class EmpleadoComponent implements OnInit {
+
+  nombre="Juan";
+
+  apellido="Díaz";
+
+  edad=28;
+
+  empresa="Píldoras Informaticas";
+
+  
+
+  /*getEdad() {
+
+    return this.edad;
+  }*/
+
+  habilitacionCuadro=false;
+
+  usuRegistrado=false;
+
+  textoDeRegistro="No hay nadie registrado";
+
+
+  getRegistroUsuario() {
+
+    this.usuRegistrado=false;
+  } 
+
+
+  setusuarioRegistrado(event:Event) {
+
+    //alert ("El usuario se acaba de registrar");
+
+    //this.textoDeRegistro="El usuario se acaba de registrar";
+
+    if((<HTMLInputElement>event.target).value=="si") {
+
+      this.textoDeRegistro="El usuario se acaba de registrar";
+      
+    }else{
+
+      this.textoDeRegistro="No hay nadie registrado";
+    }
+
+  }
 
   constructor() { }
 
